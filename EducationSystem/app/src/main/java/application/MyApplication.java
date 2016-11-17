78bbc0737d5e27a0ae884b304c5c902bf6fb022b
tianyun chen <tianyun chen>
2016-11-16 21:16:40 -0800

    push

--- EducationSystem/app/src/main/java/application/MyApplication.java
+++ EducationSystem/app/src/main/java/application/MyApplication.java
@@ -4,9 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
-import com.alibaba.sdk.android.push.CloudPushService;
-import com.alibaba.sdk.android.push.CommonCallback;
-import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
+import com.tencent.android.tpush.XGPushBaseReceiver;
+import com.tencent.android.tpush.XGPushManager;
 
 /**
  * Created by tianyun chen on 2016/11/15.
@@ -17,23 +16,11 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        initChannle(this);
+        initChannel();
     }
 
-    private void initChannle(Context application){
-        PushServiceFactory.init(application);
-        final CloudPushService pushService =PushServiceFactory.getCloudPushService();
-        pushService.register(application, new CommonCallback() {
-            @Override
-            public void onSuccess(String s) {
-                Log.d(TAG, "init cloudchannel success"+pushService.getDeviceId());
 
-            }
-
-            @Override
-            public void onFailed(String errorCode, String errorMessage) {
-                Log.d(TAG, "init cloudchannel failed -- errorcode:" + errorCode + " -- errorMessage:" + errorMessage);
-            }
-        });
+    private void initChannel(){
+        XGPushManager.registerPush(getApplicationContext(),"Arvin1");
     }
 }
